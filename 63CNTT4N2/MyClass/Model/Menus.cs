78f5ebe8179c8662ce78301d5c6b5725c5da322f
NgoPhanNhuVinh <vinh.npn.63cntt@ntu.edu.vn>
2023-11-23 19:43:41 +0700

    Menu

--- 63CNTT4N2/MyClass/Model/Menus.cs
+++ 63CNTT4N2/MyClass/Model/Menus.cs
@@ -7,39 +7,54 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MyClass.Model
-{//chua xong
-    //khai bao ten bang
+{
     [Table("Menus")]
     public class Menus
     {
-        // khai bao truong, khoa chinh
         [Key]
         public int Id { get; set; }
-        [Required]// khong dc null
+
+        [Required(ErrorMessage = "Tên menu không để trống")]
+        [Display(Name = "Tên Menu")]
         public string Name { get; set; }
 
+        [Display(Name = "Bảng dữ liệu")]
         public int? TableId { get; set; }
 
+        [Display(Name = "Kiểu Menu")]
         public string TypeMenu { get; set; }
 
+        [Display(Name = "Vị trí")]
         public string Position { get; set; }
 
+        [Display(Name = "Liên kết")]
         public string Link { get; set; }
 
+        [Display(Name = "Cấp cha")]
         public int? Parentid { get; set; }
 
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
-        [Required]
-        public int CreateBy { get; set; }
-        [Required]
+
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
         public DateTime CreateAt { get; set; }
 
-        [Required]
-        public int UpdateBy { get; set; }
-        [Required]
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
+        public int CreateBy { get; set; }
+
+        [Display(Name = "Ngày cập nhật")]
+        [Required(ErrorMessage = "Ngày cập nhật không để trống")]
         public DateTime UpdateAt { get; set; }
 
-        [Required]
+        [Display(Name = "Người cập nhật")]
+        [Required(ErrorMessage = "Người cập nhật không để trống")]
+        public int UpdateBy { get; set; }
+
+        [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái không để trống")]
         public int Status { get; set; }
+
     }
 }
